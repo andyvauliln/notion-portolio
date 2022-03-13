8fa56c93ab7fbd887ca40bfb1ae7d80e09d108f1
Andrei Vaulin <andrei@blockwatch.cc>
2022-03-13 18:47:27 +0800

    Init

---
+++ site.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  // default 78fc5a4b88d74b0e824e29407e9f1ec1
+  // all temlates 15aea51fe66743dd83fa27fadd32c94d?
+  // Test 92a966e953f345a79a4a5f82d5e0c599
+  // rootNotionPageId: 'a482527869424d269e1c8e73424df976',
+  rootNotionPageId: '067dd719a912471ea9a3ac10710e7fdf',
+ 
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'News Portal',
+  domain: 'duniakripto.notion.site',
+  author: 'Duniakripto',
+
+  // open graph metadata (optional)
+  description: 'Example site description',
+  socialImageTitle: 'Transitive Bullshit',
+  socialImageSubtitle: 'Hello World! 👋',
+
+  // social usernames (optional)
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
