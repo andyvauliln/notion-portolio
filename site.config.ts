c717887cecce5202d72bdd3ffffb567862117123
Andrei Vaulin <andrei@blockwatch.cc>
2022-05-20 19:08:53 +0800

    fixes

--- site.config.ts
+++ site.config.ts
@@ -34,7 +34,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: true,
+  isRedisEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -49,7 +49,7 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
  // navigationStyle: 'default'
-  navigationStyle: 'custom',
+  navigationStyle: 'default',
   navigationLinks: [
     {
       title: 'Home',
